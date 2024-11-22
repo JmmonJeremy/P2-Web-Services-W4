bdb57ba3eb7a4b6a0a397d10837c5684dce19403
JmmonJeremy <suc19002@byui.edu>
2024-11-21 16:31:47 -0800

    Try removing meethod from auth middleware.

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,28 +6,36 @@ const CircularJSON = require('circular-json');
 
 module.exports = {
   ensureAuth: function (req, res, next) {
-    // console.log("res.query: " + CircularJSON.stringify(res, null, 2));
-    // console.log("req.query:", req.query); 
-    console.log("Cookie: connect.sid=" + req.cookies['connect.sid']); //cookie-parser in index.js prevents Google crash
-    // console.log("Authorization: " + req.headers['authorization']); //works only for GitHub
-    console.log("Authorization: Bearer " + req.accessToken);
-    // console.log("res.locals:", res.locals); 
-    // Check if `req.user` exists before accessing `id`
-    if (req.user) {
-      console.log(`Authenticated user: ${req.user.id} |-> ${req.user.displayName}`);
+    if (req.isAuthenticated()) {
+      return next()
     } else {
-      console.log("User not found in session");
-    }  
-    if (req.isAuthenticated()) {     
-      return next();
-    } else {
-      console.log("AUTHENTICATION FAILED in ensureAuth function & redirected to 401 Unauthorized Page");
-      res.status(401).render('login', { 
-        accessDenied: true,
-        layout: 'login'  // Specifies the layout to use
-       });     
+      res.redirect('/')
     }
   },
+// module.exports = {
+//   ensureAuth: function (req, res, next) {
+//     // console.log("res.query: " + CircularJSON.stringify(res, null, 2));
+//     // console.log("req.query:", req.query); 
+//     console.log("Cookie: connect.sid=" + req.cookies['connect.sid']); //cookie-parser in index.js prevents Google crash
+//     // console.log("Authorization: " + req.headers['authorization']); //works only for GitHub
+//     console.log("Authorization: Bearer " + req.accessToken);
+//     // console.log("res.locals:", res.locals); 
+//     // Check if `req.user` exists before accessing `id`
+//     if (req.user) {
+//       console.log(`Authenticated user: ${req.user.id} |-> ${req.user.displayName}`);
+//     } else {
+//       console.log("User not found in session");
+//     }  
+//     if (req.isAuthenticated()) {     
+//       return next();
+//     } else {
+//       console.log("AUTHENTICATION FAILED in ensureAuth function & redirected to 401 Unauthorized Page");
+//       res.status(401).render('login', { 
+//         accessDenied: true,
+//         layout: 'login'  // Specifies the layout to use
+//        });     
+//     }
+//   },
 
   ensureGuest: function (req, res, next) {
     if (!req.isAuthenticated()) {
