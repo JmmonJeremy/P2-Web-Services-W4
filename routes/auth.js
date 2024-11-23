2c0568e0048eb3c82546bf56999fa4dcc07a7cb8
JmmonJeremy <suc19002@byui.edu>
2024-11-23 12:34:06 -0800

    Commented out added code part 2 to test which part of the code allowed the successful OAuth sign in for GitHub

--- routes/auth.js
+++ routes/auth.js
@@ -41,7 +41,7 @@ routes.post(
           console.error('Error during login:', loginErr);
           return next(loginErr);
         }
-        console.log('User successfully logged in:', user);
+        // console.log('User successfully logged in:', user);
         // Redirect to the desired page after successful login
         return res.redirect('/dashboard');
       });
@@ -68,9 +68,9 @@ routes.get(
     //   next(); // Pass control to the next middleware
     // },
     (req, res, next) => {
-      console.log('FROM GOOGLE CB- Authenticated user:', req.user);  // Check if the user is authenticated
-      console.log('FROM GOOGLE CB- Session set after OAuth:', req.session); // Check if the session is properly set here
-      console.log('FROM GOOGLE CB- Set-Cookie header:', res.get('Set-Cookie')); // Check if the session cookie is in the response headers
+      // console.log('FROM GOOGLE CB- Authenticated user:', req.user);  // Check if the user is authenticated
+      // console.log('FROM GOOGLE CB- Session set after OAuth:', req.session); // Check if the session is properly set here
+      // console.log('FROM GOOGLE CB- Set-Cookie header:', res.get('Set-Cookie')); // Check if the session cookie is in the response headers
       passport.authenticate('google', 
         (err, user, info) => {
           if (err) {
@@ -87,7 +87,7 @@ routes.get(
               console.error('Error during login:', loginErr);
               return next(loginErr);
             }
-            console.log('User successfully logged in:', user);
+            // console.log('User successfully logged in:', user);
             // Redirect to the desired page after successful login
             return res.redirect('/dashboard');
           });
@@ -114,15 +114,15 @@ routes.get(
   // #swagger.ignore = true
   // don't send to swagger docs it is not funtional by itself
   '/github/callback', auth.checkGithubCode, 
-    (req, res, next) => {
-      console.log('Request body:', req.body);   
-      next(); // Pass control to the next middleware
-    },
+    // (req, res, next) => {
+    //   console.log('Request body:', req.body);   
+    //   next(); // Pass control to the next middleware
+    // },
     (req, res, next) => {
     // res.setHeader('Set-Cookie', 'Cookie: connect.sid=s:QYwC5fTkJrq_m3rVFtc5cysImTWlqq0D.+VaqJWa9whZNxMNyUa1KyrreUnyArTQxVmay01hDfO4; Max-Age=24 * 60 * 60 * 1000; path=/; secure=true;');
-    console.log('FROM GITHUB CB- Authenticated user:', req.user);  // Check if the user is authenticated
-    console.log('FROM GITHUB CB- Session set after OAuth:', req.session); // Check if the session is properly set here
-    console.log('FROM GITHUB CB- Set-Cookie header:', res.get('Set-Cookie')); // Check if the session cookie is in the response headers
+    // console.log('FROM GITHUB CB- Authenticated user:', req.user);  // Check if the user is authenticated
+    // console.log('FROM GITHUB CB- Session set after OAuth:', req.session); // Check if the session is properly set here
+    // console.log('FROM GITHUB CB- Set-Cookie header:', res.get('Set-Cookie')); // Check if the session cookie is in the response headers
     passport.authenticate('github',  
       (err, user, info) => {
         if (err) {
@@ -134,17 +134,17 @@ routes.get(
           return res.redirect('/?error=Invalid credentials');
         }
         // Log in the user
-        req.logIn(user, (loginErr) => {
-          if (loginErr) {
-            console.error('Error during login:', loginErr);
-            return next(loginErr);
-          }
+        // req.logIn(user, (loginErr) => {
+        //   if (loginErr) {
+        //     console.error('Error during login:', loginErr);
+        //     return next(loginErr);
+        //   }
           console.log('User successfully logged in:', user);
           // Redirect to the desired page after successful login
           return res.redirect('/dashboard');
         });
       })(req, res, next);
-    });
+    // });
 //     { failureRedirect: '../error/401' }); 
 //     // console.log("res.query: " + CircularJSON.stringify(res, null, 2));
 //     res.status(200).redirect('/dashboard');
