27401f90ff4fc3387d98863f1a4f536f011b7f23
JmmonJeremy <suc19002@byui.edu>
2024-11-23 13:19:32 -0800

    Getting code polished up for final run with the routes/ auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -43,7 +43,7 @@ routes.post(
         }
         // console.log('User successfully logged in:', user);
         // Redirect to the desired page after successful login
-        return res.redirect('/dashboard');
+        return res.status(200).res.redirect('/dashboard');
       });
     })(req, res, next);
   });
@@ -63,10 +63,11 @@ routes.get(
   // #swagger.ignore = true
   // don't send to swagger docs it is not funtional by itself
   '/google/callback', auth.checkGoogleCode, 
-    // (req, res, next) => {
-    //   console.log('Request body:', req.body);   
-    //   next(); // Pass control to the next middleware
-    // },
+    // show what is in the request body for authentication
+    (req, res, next) => {
+      console.log('Request body:', req.body);   
+      next(); // Pass control to the next middleware
+    },
     (req, res, next) => {
       // console.log('FROM GOOGLE CB- Authenticated user:', req.user);  // Check if the user is authenticated
       // console.log('FROM GOOGLE CB- Session set after OAuth:', req.session); // Check if the session is properly set here
@@ -81,7 +82,11 @@ routes.get(
             // Redirect or render login page with an error message
             return res.redirect('/?error=Invalid credentials');
           }
-          // Log in the user
+// !!!!!!!!!!!!!!!!!!!! Log in the user (This was the code fix for OAuth to work in production)
+// req.logIn (or its alias req.login) is a Passport.js function. It is added to the req object by Passport.js middleware and is
+// used to establish a login session for a user after successful authentication. It is typically used in custom authentication flows 
+// where you handle authentication manually instead of relying entirely on Passport's built-in passport.authenticate middleware.
+// This allows you to be able to handle login errors in a more controlled way instead of letting Passport handle them automatically.
           req.logIn(user, (loginErr) => {
             if (loginErr) {
               console.error('Error during login:', loginErr);
@@ -89,7 +94,7 @@ routes.get(
             }
             // console.log('User successfully logged in:', user);
             // Redirect to the desired page after successful login
-            return res.redirect('/dashboard');
+            return res.status(200).res.redirect('/dashboard');
           });
         })(req, res, next);
       });
@@ -97,8 +102,8 @@ routes.get(
 //     res.status(200).redirect('/dashboard');
 //   }
 // );
-
 // END ********************************** GOOGLE OAUTH *********************************** END//
+
 // START ******************************** GITHUB OAUTH *********************************** START//
 // @desc    Auth with GitHub
 // @route   GET /auth/github - from "Authenticate Requests" section
@@ -114,42 +119,47 @@ routes.get(
   // #swagger.ignore = true
   // don't send to swagger docs it is not funtional by itself
   '/github/callback', auth.checkGithubCode, 
-    // (req, res, next) => {
-    //   console.log('Request body:', req.body);   
-    //   next(); // Pass control to the next middleware
-    // },
+    // show what is in the request body for authentication
+    (req, res, next) => {
+      console.log('Request body:', req.body);   
+      next(); // Pass control to the next middleware
+    },
     (req, res, next) => {
     // res.setHeader('Set-Cookie', 'Cookie: connect.sid=s:QYwC5fTkJrq_m3rVFtc5cysImTWlqq0D.+VaqJWa9whZNxMNyUa1KyrreUnyArTQxVmay01hDfO4; Max-Age=24 * 60 * 60 * 1000; path=/; secure=true;');
     // console.log('FROM GITHUB CB- Authenticated user:', req.user);  // Check if the user is authenticated
     // console.log('FROM GITHUB CB- Session set after OAuth:', req.session); // Check if the session is properly set here
     // console.log('FROM GITHUB CB- Set-Cookie header:', res.get('Set-Cookie')); // Check if the session cookie is in the response headers
     passport.authenticate('github',  
-    //   (err, user, info) => {
-    //     if (err) {
-    //       console.error('Error during authentication:', err);
-    //       return next(err);
-    //     }
-    //     if (!user) {
-    //       // Redirect or render login page with an error message
-    //       return res.redirect('/?error=Invalid credentials');
-    //     }
-    //     // Log in the user
-    //     req.logIn(user, (loginErr) => {
-    //       if (loginErr) {
-    //         console.error('Error during login:', loginErr);
-    //         return next(loginErr);
-    //       }
-    //       console.log('User successfully logged in:', user);
-    //       // Redirect to the desired page after successful login
-    //       return res.redirect('/dashboard');
-    //     });
-    //   })(req, res, next);
-    // });
-    { failureRedirect: '../error/401' }); 
-    // console.log("res.query: " + CircularJSON.stringify(res, null, 2));
-    res.status(200).redirect('/dashboard');
-  }
-);
+      (err, user, info) => {
+        if (err) {
+          console.error('Error during authentication:', err);
+          return next(err);
+        }
+        if (!user) {
+          // Redirect or render login page with an error message
+          return res.redirect('/?error=Invalid credentials');
+        }
+// !!!!!!!!!!!!!!!!!!!! Log in the user (This was the code fix for OAuth to work in production)
+// req.logIn (or its alias req.login) is a Passport.js function. It is added to the req object by Passport.js middleware and is
+// used to establish a login session for a user after successful authentication. It is typically used in custom authentication flows 
+// where you handle authentication manually instead of relying entirely on Passport's built-in passport.authenticate middleware.
+// This allows you to be able to handle login errors in a more controlled way instead of letting Passport handle them automatically.
+        req.logIn(user, (loginErr) => {
+          if (loginErr) {
+            console.error('Error during login:', loginErr);
+            return next(loginErr);
+          }
+          // console.log('User successfully logged in:', user);
+          // Redirect to the desired page after successful login
+          return res.status(200).res.redirect('/dashboard');
+        });
+      })(req, res, next);
+    });
+//     { failureRedirect: '../error/401' }); 
+//     // console.log("res.query: " + CircularJSON.stringify(res, null, 2));
+//     res.status(200).redirect('/dashboard');
+//   }
+// );
 // END ******************************** GITHUB OAUTH *********************************** END//
 
 // @desc    Logout user
