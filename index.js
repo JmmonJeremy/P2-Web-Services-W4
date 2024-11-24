d7941b5ab0ca02f3fee15b64360e4ace706df59f
JmmonJeremy <suc19002@byui.edu>
2024-11-23 22:25:21 -0800

    doc

--- index.js
+++ index.js
@@ -41,13 +41,13 @@ const options = {
       const groupA = pathA.split('/')[1]?.toLowerCase() || ''; // Default to '' if no segment
       const groupB = pathB.split('/')[1]?.toLowerCase() || ''; // Default to '' if no segment
     
-      // Prioritize 'ap' above everything else
+      // Prioritize 'r' above everything else
+      if (groupA.startsWith('r') && !groupB.startsWith('r')) return -1;
+      if (!groupA.startsWith('r') && groupB.startsWith('r')) return 1;
+
+       // Prioritize 'ap' above everything else
       if (groupA.startsWith('ap') && !groupB.startsWith('ap')) return -1;
       if (!groupA.startsWith('ap') && groupB.startsWith('ap')) return 1;
-      
-      // Prioritize 'r' above all other groups except 'ap'
-      if (groupA === 'r' && groupB !== 'ap') return -1;
-      if (groupA !== 'ap' && groupB === 'r') return 1;
     
       // Prioritize 'd' above 'c' group
       if (groupA.startsWith('d') && !groupB.startsWith('d') && groupB.startsWith('c')) return -1;
